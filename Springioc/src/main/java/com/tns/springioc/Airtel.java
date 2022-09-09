1c6ea241886c96b1ce5c0d0195bab35b1d3739a9
SHIVANAND <shivanand12546@gmail.com>
2022-09-09 16:19:13 +0530

    Spring Boot First project

---
+++ Springioc/src/main/java/com/tns/springioc/Airtel.java
@@ -0,0 +1,18 @@
+package com.tns.springioc;
+
+public class Airtel implements Sim{
+
+	public void calling() {
+		System.out.println("we are calling using Airtel Sim");
+		
+	}
+
+	public void data() {
+		
+		System.out.println("we using data from Airtel sim");
+		
+		
+	}
+	
+
+}
---
+++ Springioc/src/main/java/com/tns/springioc/Jio.java
@@ -0,0 +1,17 @@
+package com.tns.springioc;
+
+public class Jio implements Sim{
+
+	public void calling() {
+		System.out.println("calling using the jio sim");
+	
+		
+	}
+
+	public void data() {
+		
+		System.out.println("using internet from jio sim");
+	}
+	
+
+}
